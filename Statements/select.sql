5b5d1f9bff31b5c2c450f181d581287f496021ec
ElyRomero <elyoficio@gmail.com>
2025-08-03 21:32:55 -0600

    Initial commit

---
+++ Statements/select.sql
@@ -0,0 +1,7 @@
+--Muestra los datos de una tabla específica
+
+SELECT * FROM users; -- Muestra todos los datos de una tabla
+
+SELECT name FROM users; --Muestra un dato de una tabla
+
+SELECT user_id, name FROM users; --Muestra varios datos de una tabla
